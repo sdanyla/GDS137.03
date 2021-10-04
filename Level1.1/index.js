26700a6c3ec68aceaf8b4d8693cfa7c49f301346
Danyla <sdanyla@labs.neit.edu>
2021-10-04 14:47:52 -0400

    first part of 1.1

---
+++ Level1.1/index.js
@@ -0,0 +1,11 @@
+var ball = new ball();
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+context.beginPath();
+context.arc(x,y,radius,0,360*Math.PI/180,true)
+context.closePath();
+context.fill();
+
+
+
+ball();
