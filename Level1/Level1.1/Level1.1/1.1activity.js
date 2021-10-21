da2d371e3b03d6776822454da81f8dc0563d1b71
Danyla <sdanyla@labs.neit.edu>
2021-10-21 14:17:39 -0400

    working on week 1

---
+++ Level1/Level1.1/Level1.1/1.1activity.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+var canvas;
+var context;
+var timer;
+var interval = 1000/60;
+var player;
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	player = new Player();
+	
+	//------Declare the Player's speed on the x and y axis------
+	player.vx = 2;
+	player.vy = 0;
+	//----------------------------------------------------
+	
+	timer = setInterval(animate, interval);
+
+
+function animate()
---
+++ Level1/Level1.2/balllooop.js
@@ -0,0 +1,9 @@
+
+var mainTimer = setInterval(animate, 1000/60);
+function animate()
+{
+//Clear the Canvas
+//Move the Ball
+//Update the Canvas
+}
+
