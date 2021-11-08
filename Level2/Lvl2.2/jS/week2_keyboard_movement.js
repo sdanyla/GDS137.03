7a7d38af5c0d28702755a375a9fa9653617203f0
Danyla <sdanyla@email.neit.edu>
2021-11-08 12:20:09 -0500

    added folders for week 3-7

---
+++ Level2/Lvl2.2/jS/week2_keyboard_movement.js
@@ -0,0 +1,41 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new GameObject();
+
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	
+	//Move the Player up
+	if('w')
+	{
+		console.log("Moving Up");
+	player.x += 2;
+	}
+	if('s')
+	{
+		console.log("Moving Down");
+		player.x += -2;
+	}
+	
+	//Update the Screen
+	player.drawRect();
+}
+
